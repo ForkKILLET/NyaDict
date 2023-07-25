547721c2be30450f1127f52beaeac7d90c55d7ee
ForkΨKILLET <fork_killet@outlook.com>
2023-07-25 19:26:51 +0800

    feat(frontend): add parent link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 
-import BackToHome from '@comp/BackToHome.vue'
+import Topbar from '@comp/Topbar.vue'
 import Notifications from '@comp/notifications/Notifications.vue';
 
 const route = useRoute()
 </script>
 
 <template>
-    <BackToHome v-if="route.path !== '/'" />
+    <Topbar v-if="route.path !== '/'" />
     <Notifications />
     <main>
         <RouterView v-slot="{ Component }">
---
+++ frontend/src/components/Topbar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+</script>
+
+<template>
+    <div class="back-to-home">
+        <a href="javascript:void 0;" @click="router.back()">
+            <fa-icon icon="arrow-left" /> 戻る
+        </a> /
+        <RouterLink :to="route.path.replace(/\/.*?$/, '/')">
+            <fa-icon icon="arrow-up" /> アップ
+        </RouterLink>
+    </div>
+</template>
+
+<style scoped>
+.back-to-home {
+    height: 1.5rem;
+    padding: 1rem;
+}
+</style>
