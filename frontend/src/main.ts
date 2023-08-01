084fdc85fbc031c0e6633272a5aa82ef63f880fa
ForkΨKILLET <fork_killet@outlook.com>
2023-08-02 00:55:51 +0800

    refa(frontend): load fa in a single file

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,18 +3,8 @@ import { createPinia } from 'pinia'
 import { router } from './routes'
 import App from './App.vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import {
-    faCirclePlus, faArrowCircleLeft, faArrowCircleRight, faCheckCircle,
-    faTimesCircle, faArrowLeft, faArrowRight, faArrowUp, faFilter, faPenToSquare,
-    faBook, faVial, faSort, faSortUp, faSortDown, faRotate, faCalendar,
-    faFolder, faBox, faFileImport, faFileArrowDown, faFileArrowUp,
-    faFlag, faFlagCheckered, faUserCircle, faUserPlus, faRightToBracket,
-    faRightFromBracket, faLock, faTrash, faTrashRestore, faGift, faSpinner,
-    faCloud, faCloudArrowDown, faCloudArrowUp, faInfoCircle, faCircleQuestion,
-    faForward, faPlus, faChartLine, faCode, faEye,
-} from '@fortawesome/free-solid-svg-icons'
+import '@util/fa-load'
 
 import 'normalize.css'
 import '@style/index.css'
@@ -22,17 +12,6 @@ import '@style/atom.css'
 import '@style/animation.css'
 import '@style/word.css'
 import '@style/card.css'
-
-library.add(...[
-    faCirclePlus, faArrowCircleLeft, faArrowCircleRight, faCheckCircle,
-    faTimesCircle, faArrowLeft, faArrowRight, faArrowUp, faFilter, faPenToSquare,
-    faBook, faVial, faSort, faSortUp, faSortDown, faRotate, faCalendar,
-    faFolder, faBox, faFileArrowDown, faFileArrowUp, faFileImport,
-    faFlag, faFlagCheckered, faUserCircle, faUserPlus, faRightToBracket,
-    faRightFromBracket, faLock, faTrash, faTrashRestore, faGift, faSpinner,
-    faCloud, faCloudArrowUp, faCloudArrowDown, faInfoCircle, faCircleQuestion,
-    faForward, faPlus, faChartLine, faCode, faEye,
-])
 
 const pinia = createPinia()
 
---
+++ frontend/src/utils/fa-load.ts
@@ -0,0 +1,24 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+    faCirclePlus, faArrowCircleLeft, faArrowCircleRight, faCheckCircle,
+    faTimesCircle, faArrowLeft, faArrowRight, faArrowUp, faFilter, faPenToSquare,
+    faBook, faVial, faSort, faSortUp, faSortDown, faRotate, faCalendar,
+    faFolder, faBox, faFileImport, faFileArrowDown, faFileArrowUp,
+    faFlag, faFlagCheckered, faUserCircle, faUserPlus, faRightToBracket,
+    faRightFromBracket, faLock, faTrash, faTrashRestore, faGift, faSpinner,
+    faCloud, faCloudArrowDown, faCloudArrowUp, faInfoCircle, faCircleQuestion,
+    faForward, faPlus, faChartLine, faCode, faEye, faBars,
+} from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(...[
+    faCirclePlus, faArrowCircleLeft, faArrowCircleRight, faCheckCircle,
+    faTimesCircle, faArrowLeft, faArrowRight, faArrowUp, faFilter, faPenToSquare,
+    faBook, faVial, faSort, faSortUp, faSortDown, faRotate, faCalendar,
+    faFolder, faBox, faFileArrowDown, faFileArrowUp, faFileImport,
+    faFlag, faFlagCheckered, faUserCircle, faUserPlus, faRightToBracket,
+    faRightFromBracket, faLock, faTrash, faTrashRestore, faGift, faSpinner,
+    faCloud, faCloudArrowUp, faCloudArrowDown, faInfoCircle, faCircleQuestion,
+    faForward, faPlus, faChartLine, faCode, faEye, faBars,
+])
+
