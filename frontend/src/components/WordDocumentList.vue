e2d8f6976c36d06e3f6cb98583cad872e37b05fe
ForkΨKILLET <fork_killet@outlook.com>
2023-09-09 16:13:11 +0800

    feat(frontend/words): sort docs

--- frontend/src/components/WordDocumentList.vue
+++ frontend/src/components/WordDocumentList.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 import WordDocumentEntry from '@comp/WordDocumentEntry.vue'
+
 import type { IWord, IWordDocument } from '@type'
 
-defineProps<{
+const props = defineProps<{
     node: { docs?: IWordDocument[] }
     word: IWord
     hideSelf?: boolean
 }>()
+
+const sortedDocs = computed(() => {
+    const docs = [ ...props.node.docs ?? [] ]
+    return docs.sort((a, b) => a.kind - b.kind)
+})
+
+const withdrawDoc = (docId: number) => {
+    const docs = props.node.docs!
+    const index = docs.find(doc => doc.id === docId)?.id
+    if (index !== undefined) docs.splice(index, 1)
+}
 </script>
 
 <template>
     <div class="doc-list">
         <WordDocumentEntry
-            v-for="doc, index of node.docs ?? []"
-            @withdraw="node.docs?.splice(index, 1)"
+            v-for="doc of sortedDocs"
+            @withdraw="withdrawDoc(doc.id)"
             :word="word"
             :doc="doc"
             :hide-self="hideSelf"
